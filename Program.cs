b46e9e7b773cdbf878ca7bb4268eb80ef55f9d6f
Md. Tanvir Alam Anik <dell@desk.com>
2024-10-08 15:48:51 +0600

    feat: methods added

--- Program.cs
+++ Program.cs
@@ -4,5 +4,31 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () =>
+{
+    return "Api is working";
+});
+
+app.MapGet("/hi", () =>
+{
+    return "Hi from /hi route";
+});
+
+app.MapPost("/", () =>
+{
+    return "POST is working!";
+});
+
+app.MapPatch("/", () =>
+{
+    return "Patch is working!";
+});
+
+app.MapDelete("/", () =>
+{
+    return "Delete is working!";
+});
+
+
 app.Run();
 
